206f10baeba0289f0390ea826dcbbc82c337ddfc
MK-Khan123 <mehnazkhan231@gmail.com>
2021-03-19 13:29:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { createContext } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+
+export const UserContext = createContext();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <UserContext.Provider>
+        <Router>
+          <Header />
+          <Switch>
+            <Route>
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </UserContext.Provider>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import background from '../../images/city-background.png';
+import fakeData from '../../fakeData/MOCK_DATA.json';
+import './Home.css';
+
+const Home = () => {
+
+    const backgroundStyle = {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${background})`,
+        width: '100%',
+        height: '100%',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+    }
+    return (
+        <div style={backgroundStyle}>
+            <div className="row row-cols-1 row-cols-md-4 g-3" style={{ paddingTop: '300px', paddingBottom: '300px' }}>
+                {
+                    fakeData.map(data => {
+
+                        const { ride, img, id } = data;
+                        return (
+                            <Link key={id} style={{ textDecoration: 'none' }} to="/destination">
+                                <div className="col">
+                                    <div className="card cardStyle">
+                                        <img src={img} className="card-img-top" alt="..." />
+                                        <div className="card-body">
+                                            <h5 className="card-title text-uppercase">{ride}</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        );
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Home;
